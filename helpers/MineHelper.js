710f0b84f5410799fdb3450fe0d528b3009dd28e
Ethex Team <48210278+ethexbet@users.noreply.github.com>
2019-10-29 13:16:13 +0000

    Styling

--- helpers/MineHelper.js
+++ helpers/MineHelper.js
@@ -21,19 +21,19 @@ advanceTime = (time) => {
 
 advanceBlock = () => {
     return new Promise((resolve, reject) => {
-		web3.currentProvider.send({
-			jsonrpc: "2.0",
-			method: "evm_mine",
-			id: new Date().getTime()
-		}, (err, result) => {
-				if (err) { return reject(err); }
-				return resolve(result);
-		});
+        web3.currentProvider.send({
+            jsonrpc: "2.0",
+            method: "evm_mine",
+            id: new Date().getTime()
+        }, (err, result) => {
+                if (err) { return reject(err); }
+                return resolve(result);
+        });
     });
 }
 
 module.exports = {
     advanceTime,
     advanceBlock,
-	advanceTimeAndBlock
+    advanceTimeAndBlock
 }
